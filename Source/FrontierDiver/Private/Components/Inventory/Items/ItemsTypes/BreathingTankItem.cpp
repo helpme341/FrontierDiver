1d3f15e8f579cce2bcf5f22410ee22edd54019b9
helpme341 <137097948+helpme341@users.noreply.github.com>
2024-07-24 19:56:00 +0300

    Breathing Tank Base (0)

---
+++ Source/FrontierDiver/Private/Components/Inventory/Items/ItemsTypes/BreathingTankItem.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/Inventory/Items/ItemsTypes/BreathingTankItem.h"
+
+UBreathingTankItem::UBreathingTankItem()
+{
+	bIsPlayerCanDropThisItem = true;
+}
+
+void UBreathingTankItem::SetItemDynamicInfo(UItemDynamicInfo* DynamicInfo)
+{
+    if (!DynamicInfo)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("DynamicInfo is nullptr!"));
+        return;
+    }
+
+    UBreathingTankItemDynamicInfo* BreathingInfo = Cast< UBreathingTankItemDynamicInfo>(DynamicInfo);
+    if (BreathingInfo)
+    {
+        ItemDynamicInfo.Reset(BreathingInfo);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Failed to cast DynamicInfo to UFlashlightItemDynamicInfo!"));
+    }
+}
---
+++ Source/FrontierDiver/Public/Components/Inventory/Items/ItemsTypes/BreathingTankItem.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Inventory/Items/ItemBase.h"
+#include "Components/Inventory/Items/ItemTmpl.h"
+#include "BreathingTankItem.generated.h"
+
+UCLASS()
+class FRONTIERDIVER_API UBreathingTankItemDynamicInfo : public UItemDynamicInfo
+{
+	GENERATED_BODY()
+};
+
+USTRUCT(BlueprintType)
+struct FBreathingTankItemTableRowInfo : public FItemTableRowInfoBase
+{
+	GENERATED_BODY()
+
+	FBreathingTankItemTableRowInfo()
+	{
+		MaxQuantityItemsInSlot = 1;
+	}
+};
+
+
+
+/**
+ * 
+ */
+UCLASS()
+class FRONTIERDIVER_API UBreathingTankItem : public UItemBase,
+	public TItemTmpl<UBreathingTankItemDynamicInfo, FBreathingTankItemTableRowInfo>
+{
+	GENERATED_BODY()
+public:
+
+	UBreathingTankItem();
+
+	bool FindDataTableByItemType(UWorld* World)
+		override { return BaseFindDataTableByItemType<UBreathingTankItem, FBreathingTankItemTableRowInfo>(World); }
+
+	const FItemTableRowInfoBase* GetItemStaticInfo()
+		override { return ItemTableRowInfo.Get(); }
+
+	UItemDynamicInfo* GetItemDynamicInfo() 
+		override { return ItemDynamicInfo.Get(); }
+
+	void SetItemDynamicInfo(UItemDynamicInfo* DynamicInfo) override;
+};
